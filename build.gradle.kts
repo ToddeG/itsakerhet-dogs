7b9be2b0ed02b3aae02a932cf1f39c9ca1be4401
ToddeG <146707807+ToddeG@users.noreply.github.com>
2024-09-03 11:23:11 +0200

    This reverts commit 30aeac48ef025d9bdfe84f6e8795ccf7ea609361. This commit will revert the previous commit which was made mistankenly

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
     implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("com.github.javafaker:javafaker:1.0.2") { exclude ("org.yaml") }
 
     compileOnly("org.projectlombok:lombok")
